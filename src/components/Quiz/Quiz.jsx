20c43443e1d1a3ab395e31e2d9921711e4356c65
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-10-11 14:50:10 +0600

    Quized added with style, options added

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Quiz = () => {
+import { Radio } from "@mui/joy";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel from "@mui/joy/FormLabel";
+import RadioGroup from "@mui/joy/RadioGroup";
+
+const Quiz = ({ question: { question, correctAnswer, options }, idx }) => {
+  console.log("🚀 ~ file: Quiz.jsx ~ line 9 ~ Quiz ~ options", options);
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
   return (
-    <div>
-      <h1>Quiz</h1>
+    <div className="h-[300px] w-[50%] mx-auto backdrop-blur-[5px] bg-white/30 shadow-lg text-[#000A38]">
+      <FormControl>
+        <FormLabel>
+          <span className="text-[28px] font-bold">
+            Quiz {idx}: {question}
+          </span>
+        </FormLabel>
+        <RadioGroup
+          defaultValue=""
+          name="controlled-radio-buttons-group"
+          value={value}
+          onChange={handleChange}
+          sx={{ my: 2, mx: 5 }}
+        >
+          {options.map((option) => (
+            <Radio
+              className="font-medium"
+              size="lg"
+              variant="outlined"
+              value={option}
+              label={option}
+            />
+          ))}
+        </RadioGroup>
+      </FormControl>
     </div>
   );
 };
--- src/components/Quizes/Quizes.jsx
+++ src/components/Quizes/Quizes.jsx
@@ -14,11 +14,11 @@ const Quizes = () => {
   // console.log("🚀 ~ file: Quiz.jsx ~ line 12 ~ Quiz ~ logo", logo);
   return (
     <div
-      className={`bg-fixed bg-[url('https://live.staticflickr.com/65535/52413593240_e00326e727_o.png')] bg-no-repeat bg-contain h-[100vh] bg-center pt-[120px] text-center`}
+      className={`bg-fixed bg-[url('https://live.staticflickr.com/65535/52413593240_e00326e727_o.png')] bg-no-repeat bg-contain min-h-[100vh] bg-center pt-[120px]`}
     >
-      <h1 className="text-3xl font-bold">{name}</h1>
-      {questions.map((question) => (
-        <Quiz key={question.id} question={question}></Quiz>
+      <h1 className="text-3xl font-bold text-center">{name}</h1>
+      {questions.map((question, idx) => (
+        <Quiz key={question.id} question={question} idx={idx + 1}></Quiz>
       ))}
     </div>
   );
