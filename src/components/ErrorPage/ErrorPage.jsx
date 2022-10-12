17ba10522926955ff00184a1747033f5618b5358
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-10-12 17:41:57 +0600

    final update/ error page update

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -7,7 +7,7 @@ const ErrorPage = () => {
   return (
     <div className="flex justify-center items-center h-[100vh]">
       <div>
-        <h1>Opps!</h1>
+        <h1 className="text-[56px] font-extrabold">Opps!</h1>
         <p>An unexpected error occured.</p>
         <p>
           <i>
@@ -15,8 +15,8 @@ const ErrorPage = () => {
             {error.statusText || error.message}
           </i>
         </p>
-        <button className="btn">
-          <Link to={"/"}>Back to Home</Link>
+        <button className="px-5 py-2 font-bold mt-5 bg-white text-black rounded-lg hover:bg-slate-300">
+          <Link to={"/"}>Go to Home</Link>
         </button>
       </div>
     </div>
